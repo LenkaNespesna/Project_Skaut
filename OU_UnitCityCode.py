407b9c2f4850e8ad03ecb077edb8e68324bff46d
DubanevN <92751217+DubanevN@users.noreply.github.com>
2024-06-02 21:30:48 +0200

    Update OU_UnitCityCode.py

--- OU_UnitCityCode.py
+++ OU_UnitCityCode.py
@@ -1,5 +1,4 @@
 import csv
-import pandas as pd
 
 # Otevřu si tabulku D_GEO jako json, tedy seznam slovníků
 cities = []
@@ -8,28 +7,6 @@ with open("in/tables/D_GEO.csv", encoding="utf-8") as dGeo:
     for line in reader:
         cities.append(line)
 
-# Otevřu si tabulku s populací obcí v roce 2024
-PocetObyvatel = pd.read_csv("in/tables/PocetObyvatel2024.csv")
-
-# Určím si primární klíč
-kod_obce = "kod_obce"
-
-cities_df = pd.DataFrame(cities)
-
-# Přejmenuju sloupec "kodobce", aby odpovídal primálnímu klíči
-cities_df = cities_df.rename(columns={"kodobce": kod_obce})
-
-# Převedu oba kody obcí na stringy
-cities_df[kod_obce] = cities_df[kod_obce].astype(str)
-PocetObyvatel[kod_obce] = PocetObyvatel[kod_obce].astype(str)
-
-# Spojím tabulky podle primárního klíče
-merged_cities = pd.merge(
-    cities_df, PocetObyvatel[[kod_obce, "celkem"]], on=kod_obce, how="left"
-)
-
-# Převedu do slovníku
-merged_cities_dict = merged_cities.to_dict(orient="records")
 
 # Otevřu si tabulku OU_Unit jako seznam slovníků
 units = []
@@ -38,64 +15,40 @@ with open("in/tables/OU_Unit.csv", encoding="utf-8") as dUnit:
     for row in readerUnit:
         units.append(row)
 
-# Otevřu si tabulku s počtem členů za různé roky
-UnitRegistration = pd.read_csv("in/tables/OU_UnitRegistration.csv")
-#Převedu na string sloupec "ID_Unit"
-UnitRegistration["ID_Unit"] = UnitRegistration["ID_Unit"].astype(str)
-# Vyfiltruji si jen řádky z roku 2024
-UnitRegistration24 = UnitRegistration[UnitRegistration["Year"] == 2024]
-
-# Určím si primární klíč
-ID_Unit = "ID_Unit"
-
-units_df = pd.DataFrame(units)
-#Přejmenuju sloupec "ID" na "ID_Unit" podle primátního klíče
-units_df = units_df.rename(columns={"ID": ID_Unit})
-
-#Převedu na string, abych mohla spojit obě tabulky
-units_df[ID_Unit] = units_df[ID_Unit].astype(str)
-
-# Spojím tabulky podle primárního klíče
-
-merged_units = pd.merge(
-    units_df, UnitRegistration24[[ID_Unit, "RegularMembers"]], on=ID_Unit, how="left"
-)
-merged_units_dict = merged_units.to_dict(orient="records")
-
-for unit in merged_units_dict:
+for unit in units:
     match_found = False
-    for city in merged_cities_dict:
+    for city in cities:
         # Pokud je vyplněné i město i psč, vyhledá shodu a doplní kód obce
-        if city["nazobce"] in unit.get("City") and city["psc"] == unit.get("Postcode") and unit["RegularMembers"] < city["celkem"]:
+        if city["nazobce"] in unit.get("City"):
             # Přidám ke každému slovníku klíč kodobce a její hodnotu
-            unit["kod_obce"] = city["kod_obce"]
+            unit["kodobce"] = city["kodobce"]
             match_found = True
             break
         # Pokud chybí vyplněné PSČ a máme jen město, napáruji pomocí názvu města
-        elif city["nazobce"] in unit.get("City") and unit["RegularMembers"] < city["celkem"]:
-            unit["kod_obce"] = city["kod_obce"]
+        elif city["nazobce"] in unit.get("City"):
+            unit["kodobce"] = city["kodobce"]
             match_found = True
             break
         # Pokud chybí i město v tabulce OU_Unit, napáruji pomocí "Location"
-        elif city["nazobce"] in unit.get("Location") and unit["RegularMembers"] < city["celkem"]:
-            unit["kod_obce"] = city["kod_obce"]
+        elif city["nazobce"] in unit.get("Location"):
+            unit["kodobce"] = city["kodobce"]
             match_found = True
             break
         # Někde mám jen informaci o o PSČ, napáruji pomocí něho
-        elif city["psc"] == unit.get("Postcode") and unit["RegularMembers"] < city["celkem"]:
-            unit["kod_obce"] = city["kod_obce"]
+        elif city["psc"] == unit.get("Postcode"):
+            unit["kodobce"] = city["kodobce"]
             match_found = True
             break
         # Někdy se název obce z ČSÚ nachází ve sloupci "nazev_casti_obce"
         elif (city["nazev_casti_obce"] in unit.get("City") or city[
             "nazev_casti_obce"
-        ] in unit.get("Location")) and unit["RegularMembers"] < city["celkem"]:
-            unit["kod_obce"] = city["kod_obce"]
+        ] in unit.get("Location")):
+            unit["kodobce"] = city["kodobce"]
             match_found = True
             break
     # Pokud nelze spárovat, dám None
     if match_found == False:
-        unit["kod_obce"] = "Není dostupné"
+        unit["kodobce"] = "Není dostupné"
 
 
 # filename = "OU_UnitCityCode.csv"
@@ -111,7 +64,7 @@ fieldnames = [
     "City",
     "Postcode",
     "State",
-    "kod_obce",
+    "kodobce",
 ]
 
 with open(
