20f580812111a038b2e379cf5723e4827af7739b
DubanevN <92751217+DubanevN@users.noreply.github.com>
2024-06-02 13:18:44 +0200

    Update D_PersonRegistration.sql

--- D_PersonRegistration.sql
+++ D_PersonRegistration.sql
@@ -5,11 +5,11 @@ SELECT TO_NUMBER(PR."ID" )AS "ID_PersonRegistration"
     ,TO_NUMBER(PR."ID_Person") AS "ID_Person"
     ,TO_NUMBER(PR."ID_UnitRegistration") AS "ID_UnitRegistration"
     , CASE 
-WHEN "ID_UnitType" = 'oddil' THEN UR."ID"::NUMBER
+WHEN "ID_UnitType" = 'oddil' THEN UR."ID_UnitRegistration"::NUMBER
 WHEN "ID_UnitType" = 'druzina' THEN UR."ID_UnitRegistrationParent"::NUMBER
 ELSE NULL
 END AS "ID_UnitOddil" 
 FROM "OU_PersonRegistration" PR
-LEFT JOIN "F_UnitRegistration" UR ON PR."ID_UnitRegistration" = UR."ID"
+LEFT JOIN "F_UnitRegistration" UR ON PR."ID_UnitRegistration" = UR."ID_UnitRegistration"
 LEFT JOIN "D_Unit" U on UR."ID_Unit" = U."ID_Unit"
 ;
